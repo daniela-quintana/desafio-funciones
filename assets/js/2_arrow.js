4ce7b871f9545ffb024ebe0b6e6d393f95254568
Daniela Quintana <51489238+daniela-quintana@users.noreply.github.com>
2025-09-07 23:37:52 -0300

    first commit

---
+++ assets/js/2_arrow.js
@@ -0,0 +1,10 @@
+// Transforma la siguiente función a una arrow function de una línea. Este ejercicio
+// debe quedar en un archivo llamado `2_arrow.js` (1 Punto).
+
+let suma = function(a, b){
+return a + b
+}
+
+// Respuesta:
+
+const suma2 = (a, b) => a + b;
